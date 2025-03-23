5746ab93cff46b6185c65d582fac6c18bcc3280a
david-o-rosario1201 <finnnn023@gmail.com>
2025-03-23 11:11:46 -0400

    Agrega condicion y dependiendo de si el estudiante esta loggeado o no navega hacia esa pantalla

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:ucne_guide/Presentation/home_screen.dart';
+import 'package:ucne_guide/Presentation/inicio_sesion_screen.dart';
+import 'package:ucne_guide/Presentation/registro_screen.dart';
+
+import 'SharedPreferences/sharedPreferencesService.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HomeScreen(),
+    home: VerificarUsuarioLoggeado(),
   ));
 }
+
+class VerificarUsuarioLoggeado extends StatefulWidget {
+  const VerificarUsuarioLoggeado({super.key});
+
+  @override
+  State<VerificarUsuarioLoggeado> createState() => _VerificarUsuarioLoggeadoState();
+}
+
+class _VerificarUsuarioLoggeadoState extends State<VerificarUsuarioLoggeado> {
+
+  final SharedPreferencesService _prefsService = SharedPreferencesService();
+  String? estudiante = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    String username = await _prefsService.loadUsername();
+    setState(() {
+      estudiante = username;
+    });
+    Navegar();
+  }
+
+  void Navegar() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (estudiante == "No user saved") {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => InicioSesionScreen()),
+        );
+      } else {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreen()),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
