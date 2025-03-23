002392f6236f4b8a89a45a5534824ad3e9ea9633
david-o-rosario1201 <finnnn023@gmail.com>
2025-03-23 11:11:03 -0400

    Agrega la llamada a share preferences para obtener los datos del usuario loggeado

--- lib/Presentation/home_screen.dart
+++ lib/Presentation/home_screen.dart
@@ -3,6 +3,9 @@ import 'package:ucne_guide/Presentation/consulta_asignatura_screen.dart';
 import 'package:ucne_guide/Presentation/consulta_maestro_screen.dart';
 import 'package:ucne_guide/Presentation/drawer_menu.dart';
 
+import '../SharedPreferences/sharedPreferencesService.dart';
+import '../api/api_service.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -11,6 +14,29 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final Api_Service apiService = Api_Service();
+  final SharedPreferencesService _prefsService = SharedPreferencesService();
+
+  String estudiante = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+    //_deleteData();
+  }
+
+  Future<void> _loadData() async {
+    String username = await _prefsService.loadUsername();
+    setState(() {
+      estudiante = username;
+    });
+  }
+
+  Future<void> _deleteData() async {
+    await _prefsService.clearAllData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return DrawerMenu(
@@ -18,23 +44,38 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            _buildCard("Consulta de Materias", () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ConsultaAsignaturaScreen()),
-              );
-            }),
-            _buildCard("Consulta de Maestros", () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ConsultaMaestroScreen()),
-              );
-            }),
-            _buildCard("Calificar Maestro", () {})
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 40),
+                Text(
+                  "Bienvenido, ${estudiante}",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueAccent,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 30),
+                _buildCard("Consulta de Materias", () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ConsultaAsignaturaScreen()),
+                  );
+                }),
+                _buildCard("Consulta de Maestros", () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ConsultaMaestroScreen()),
+                  );
+                }),
+                _buildCard("Calificar Maestro", () {})
+              ],
+            )
           ],
-        ),
+        )
       ),
     );
   }
