6137938408ff07e6d852ab3f6896f17ed86d6cc8
david-o-rosario1201 <finnnn023@gmail.com>
2025-04-05 19:05:10 -0400

    Agrega el nuevo atributo "comentario"

--- lib/Modelos/maestros.dart
+++ lib/Modelos/maestros.dart
@@ -4,18 +4,21 @@ class Maestros{
   final int maestroId;
   final String nombre;
   final int materiaId;
+  final String comentario;
 
   Maestros({
     required this.materiaId,
     required this.nombre,
-    required this.maestroId
+    required this.maestroId,
+    required this.comentario
   });
 
   factory Maestros.fromJson(Map<String, dynamic> json) {
     return Maestros(
         maestroId: json['maestroid'],
         nombre: utf8.decode(json['nombre'].toString().codeUnits),
-        materiaId: json['materiaid'] ?? 0
+        materiaId: json['materiaid'] ?? 0,
+        comentario: utf8.decode(json['nombre'].toString().codeUnits)
     );
   }
 
@@ -24,6 +27,7 @@ class Maestros{
       'maestroid': maestroId,
       'nombre': nombre,
       'materiaid': materiaId,
+      'comentario': comentario
     };
   }
 }
